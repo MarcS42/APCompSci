e23356c5b2a75ca3f52d080c0de337c6440361b8
MarcSherman <MarcSherman@192.168.0.24>
2018-01-24 15:01:07 -0800

    APCompSci Free-writing Exam#2 Pgm#3 first push Machine class, answer part (a).

---
+++ APCompSci/Machine.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+/**
+ * @author MarcSherman
+ *
+ */
+public class Machine {
+    
+    private PaperRoll paper;
+    
+    public Machine(PaperRoll roll) {
+        paper = roll;
+    }
+    
+    public PaperRoll getPaperRoll() {
+        return paper;
+    }
+    
+    /**Returns the current partial roll 
+     *           and replaces it with the new roll
+     * 
+     * @param pRoll - a new full PaperRoll
+     * @return PaperRoll the old nearly empty PaperRoll 
+     */
+    public PaperRoll replacePaper(PaperRoll pRoll) {
+        PaperRoll usedRoll = getPaperRoll();
+        paper = pRoll;
+        
+        return usedRoll;
+    }//end replacePaper method
+
+
+}// End Machine class
