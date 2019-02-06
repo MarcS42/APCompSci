530ddb8fddbb28b627f726d05e076b70ac256704
MarcSherman <MarcSherman@192.168.254.21>
2019-02-05 20:29:06 -0800

    CodeQuest Numerical Order problem

---
+++ APCompSci/APCompSciProblems/CodeQuest3PlusMinus.java
@@ -0,0 +1,49 @@
+package APCompSciProblems;
+
+public class CodeQuest3PlusMinus {
+  
+  public String ascendingDescending(String[] strVals)
+  {
+    String plusMinus="";
+    int elemPrev, elemNext;
+    for(int i=0; i<strVals.length-1;i++)
+    {
+        elemPrev = Integer.parseInt(strVals[i]);
+        elemNext = Integer.parseInt(strVals[i+1]);
+        if(elemPrev<elemNext)
+        {
+            plusMinus=plusMinus+"+";    
+        }else 
+          if(elemPrev>elemNext)
+            {
+                plusMinus=plusMinus+"-";
+            }
+    }
+    
+    if(plusMinus.indexOf("+")<0)
+    {
+        return "The numbers are in descending order";
+    }else 
+      if(plusMinus.indexOf("-")<0)
+      {
+        return "The numbers are in ascending order";
+      }else if(plusMinus.indexOf("+")>=0 && plusMinus.indexOf("-")>=0)
+        {
+            return "The numbers are in random order";
+        }
+    return null;
+  }
+  
+
+  public static void main(String[] args)
+  {
+    CodeQuest3PlusMinus cQ3 = new CodeQuest3PlusMinus();
+    String[] strVals = {"5","6","9","13","17","15"};
+    
+    System.out.println(cQ3.ascendingDescending(strVals));
+  }
+
+  
+
+}
+
