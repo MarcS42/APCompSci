89fa49e0b6501f28a4123d1cd41cd5689f7aad29
MarcS42 <g1enfiddich@yahoo.com>
2019-05-18 14:57:37 -0700

    Exploring Inheritance

---
+++ APCompSci/APCompSciProblems/Dancer.java
@@ -0,0 +1,16 @@
+package APCompSciProblems;
+
+public class Dancer {
+  
+  public void act()
+  {
+    System.out.println(" spin");
+    doTrick();
+  }
+  
+  public void doTrick()
+  {
+    System.out.println(" float");
+  }
+
+}
---
+++ APCompSci/APCompSciProblems/DancerRunner.java
@@ -0,0 +1,15 @@
+package APCompSciProblems;
+
+public class DancerRunner {
+
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+    Dancer a = new Acrobat();
+    a.act();
+    Acrobat b = new Acrobat();
+    b.act();
+
+  }
+
+}
