9d70e565193649f376cc2ed3c1ec917cba1b2557
MarcS42 <g1enfiddich@yahoo.com>
2019-05-18 16:50:14 -0700

    Minor Stuff

--- APCompSci/APCompSciProblems/DancerRunner.java
+++ APCompSci/APCompSciProblems/DancerRunner.java
@@ -4,9 +4,18 @@ public class DancerRunner {
 
   public static void main(String[] args)
   {
-    // TODO Auto-generated method stub
+    /**
+     * When picking a method, and IFF method
+     * has been overridden by subclass,
+     * run-time looks to instantiated object 
+     * i.e. 'new Acrobat()' to decide which 
+     * 'act()' method to call.
+     * If method is not overridden, there is no 
+     * potential conflict.
+     */
     Dancer a = new Acrobat();
     a.act();
+    System.out.println("--------");
     Acrobat b = new Acrobat();
     b.act();
 
