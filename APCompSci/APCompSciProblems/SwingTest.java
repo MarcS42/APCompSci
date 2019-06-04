ca540a5c3f0fc4902fdeef71855aaa73cd170eef
MarcS42 <g1enfiddich@yahoo.com>
2019-06-04 11:43:11 -0700

    Various

---
+++ APCompSci/APCompSciProblems/SwingTest.java
@@ -0,0 +1,21 @@
+package APCompSciProblems;
+
+//Download, read and run this program 
+import javax.swing.*; 
+public class SwingTest {     
+  public static void main(String[] args) {       
+    // Create new frame        
+    JFrame frame= new JFrame();       
+    // Tells program to exit when user closes this frame         
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);         
+    // Frame has 0 default size; give it a size         
+    frame.setSize(500, 400); // setSize(int x, int y)
+    
+    // Frame invisible by default; make it visible         
+    frame.setVisible(true);     
+  }     
+  // main() ends but Swing thread stays alive 
+  } 
+// Run the program; see what it draws 
+  
+
